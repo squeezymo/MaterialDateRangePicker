b8641d575751113a8a7fbe7aad5ee6995219b1b4
squeezymo <squeezymo@gmail.com>
2016-05-17 12:04:31 +0300

    DatePickerDialog now extends support DialogFragment

--- library/src/main/java/com/borax12/materialdaterangepicker/date/DatePickerDialog.java
+++ library/src/main/java/com/borax12/materialdaterangepicker/date/DatePickerDialog.java
@@ -18,7 +18,7 @@ package com.borax12.materialdaterangepicker.date;
 
 import android.animation.ObjectAnimator;
 import android.app.Activity;
-import android.app.DialogFragment;
+import android.support.v4.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Bundle;
